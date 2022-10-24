29fce7fb6e4a04f2108b8402f1637e6aa2915810
Pham Nguyen Nhu Binh <2054052007binh@ou.edu.vn>
2022-10-24 16:38:42 +0700

    Chinh sua code

--- MyApp/src/main/java/com/mycompany/myapp/MyApp.java
+++ MyApp/src/main/java/com/mycompany/myapp/MyApp.java
@@ -11,7 +11,8 @@ package com.mycompany.myapp;
  */
 public class MyApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+   public static void main (String[] args)
+   {
+       System.out.println("Hello World!!!!");
+   }
 }
